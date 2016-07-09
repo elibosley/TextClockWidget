c161e998732fbb014f25174ef221bbb4703583d1
Eli Bosley <ekbosley@gmail.com>
2016-07-09 10:32:05 -0400

    Getting closer Need to figure out how to get the TimeService to update

--- app/src/main/java/com/elijahbosley/textclockwidget/TextClockWidget.java
+++ app/src/main/java/com/elijahbosley/textclockwidget/TextClockWidget.java
@@ -15,6 +15,7 @@ import android.text.format.DateFormat;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import java.sql.Time;
 import java.util.Calendar;
 import java.util.List;
 
@@ -23,6 +24,7 @@ import java.util.List;
  * App Widget Configuration implemented in {@link TextClockWidgetConfigureActivity TextClockWidgetConfigureActivity}
  */
 public class TextClockWidget extends AppWidgetProvider {
+    private static String clockText = "hi";
 
     @Override
     public void onEnabled(Context context) {
@@ -31,6 +33,7 @@ public class TextClockWidget extends AppWidgetProvider {
         Intent serviceIntent = new Intent(UpdateTimeService.UPDATE_TIME);
         serviceIntent.setPackage("com.elijahbosley.textclockwidget.UpdateTimeService");
         context.startService(serviceIntent);
+
     }
 
     @Override
@@ -44,10 +47,27 @@ public class TextClockWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
+
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
         Intent serviceIntent = new Intent(UpdateTimeService.UPDATE_TIME);
         serviceIntent.setPackage("com.elijahbosley.textclockwidget.UpdateTimeService");
         context.startService(serviceIntent);
     }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        TimeString timeString = new TimeString();
+        clockText = timeString.timeAsString();
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.text_clock_widget);
+        views.setTextViewText(R.id.appwidget_text, clockText);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
 
 }
 
--- app/src/main/java/com/elijahbosley/textclockwidget/UpdateTimeService.java
+++ app/src/main/java/com/elijahbosley/textclockwidget/UpdateTimeService.java
@@ -1,58 +1,37 @@
 package com.elijahbosley.textclockwidget;
 
 import android.app.Service;
-import android.appwidget.AppWidgetManager;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
- * Created by elibosley on 7/8/16.
+ * Created by elibosley on 7/8/16
  */
 public final class UpdateTimeService extends Service {
     static final String UPDATE_TIME = "com.elijahbosley.textclock.action.UPDATE_TIME";
 
-    private Calendar mCalendar;
-    private final static IntentFilter mIntentFilter = new IntentFilter();
-
-    static {
-        mIntentFilter.addAction(Intent.ACTION_TIME_TICK);
-        mIntentFilter.addAction(Intent.ACTION_TIME_CHANGED);
-        mIntentFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
-    }
+    BroadcastReceiver _broadcastReceiver;
+    private final SimpleDateFormat _sdfWatchTime = new SimpleDateFormat("HH:mm");
+    private CharSequence _tvTime;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        mCalendar = Calendar.getInstance();
-        registerReceiver(mTimeChangedReceiver, mIntentFilter);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-
-        unregisterReceiver(mTimeChangedReceiver);
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        super.onStartCommand(intent, flags, startId);
-
-        if (intent != null) {
-            if (UPDATE_TIME.equals(intent.getAction())) {
-                updateTime();
-            }
-        }
-
-        return START_STICKY;
+        if (_broadcastReceiver != null)
+            unregisterReceiver(_broadcastReceiver);
     }
 
     @Override
@@ -60,21 +39,23 @@ public final class UpdateTimeService extends Service {
         return null;
     }
 
-    private final BroadcastReceiver mTimeChangedReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            updateTime();
-        }
-    };
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
 
-    private void updateTime() {
-        mCalendar.setTimeInMillis(System.currentTimeMillis());
+        _broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context ctx, Intent intent) {
+                if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0)
+                    _tvTime = (_sdfWatchTime.format(new Date()));
+                    RemoteViews views = new RemoteViews(getApplicationContext().getPackageName(), R.layout.text_clock_widget);
+                    views.setTextViewText(R.id.appwidget_text, _tvTime);
+            }
+        };
 
-        RemoteViews mRemoteViews = new RemoteViews(getPackageName(), R.layout.text_clock_widget);
-        TimeString timeString = new TimeString();
-        mRemoteViews.setTextViewText(R.id.appwidget_text, timeString.timeAsString());
-        ComponentName mComponentName = new ComponentName(this, TextClockWidget.class);
-        AppWidgetManager mAppWidgetManager = AppWidgetManager.getInstance(this);
-        mAppWidgetManager.updateAppWidget(mComponentName, mRemoteViews);
+        registerReceiver(_broadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+
+        return 1;
     }
+
 }
