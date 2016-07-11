f2fcf4d7b4e08ffbce339c5aac0165494c54b089
Eli Bosley <ekbosley@gmail.com>
2016-07-10 23:04:53 -0400

    TIME UPDATING WORKS!

--- app/src/main/java/com/elijahbosley/textclockwidget/TextClockWidget.java
+++ app/src/main/java/com/elijahbosley/textclockwidget/TextClockWidget.java
@@ -2,15 +2,18 @@ package com.elijahbosley.textclockwidget;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
+import android.os.IBinder;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.widget.RemoteViews;
@@ -24,16 +27,13 @@ import java.util.List;
  * App Widget Configuration implemented in {@link TextClockWidgetConfigureActivity TextClockWidgetConfigureActivity}
  */
 public class TextClockWidget extends AppWidgetProvider {
-    private static String clockText = "hi";
 
     @Override
     public void onEnabled(Context context) {
+
         super.onEnabled(context);
 
-        Intent serviceIntent = new Intent(UpdateTimeService.UPDATE_TIME);
-        serviceIntent.setPackage("com.elijahbosley.textclockwidget.UpdateTimeService");
-        context.startService(serviceIntent);
-
+        context.startService(new Intent(UpdateTimeService.UPDATE_TIME));
     }
 
     @Override
@@ -47,25 +47,72 @@ public class TextClockWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
 
-
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
-        Intent serviceIntent = new Intent(UpdateTimeService.UPDATE_TIME);
-        serviceIntent.setPackage("com.elijahbosley.textclockwidget.UpdateTimeService");
-        context.startService(serviceIntent);
+        context.startService(new Intent(UpdateTimeService.UPDATE_TIME));
     }
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-        TimeString timeString = new TimeString();
-        clockText = timeString.timeAsString();
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.text_clock_widget);
-        views.setTextViewText(R.id.appwidget_text, clockText);
+    public static final class UpdateTimeService extends Service {
+        static final String UPDATE_TIME = "org.coax.widget.digitalclock.action.UPDATE_TIME";
 
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+        private Calendar mCalendar;
+        private final static IntentFilter mIntentFilter = new IntentFilter();
+
+        static {
+            mIntentFilter.addAction(Intent.ACTION_TIME_TICK);
+            mIntentFilter.addAction(Intent.ACTION_TIME_CHANGED);
+            mIntentFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+        }
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+
+            mCalendar = Calendar.getInstance();
+            registerReceiver(mTimeChangedReceiver, mIntentFilter);
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+
+            unregisterReceiver(mTimeChangedReceiver);
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            super.onStartCommand(intent, flags, startId);
+
+            if (intent != null) {
+                if (UPDATE_TIME.equals(intent.getAction())) {
+                    updateTime();
+                }
+            }
+
+            return START_STICKY;
+        }
+
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        private final BroadcastReceiver mTimeChangedReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                updateTime();
+            }
+        };
+
+        private void updateTime() {
+            mCalendar.setTimeInMillis(System.currentTimeMillis());
+            TimeString timeString = new TimeString();
+            String time = timeString.timeAsString();
+            RemoteViews mRemoteViews = new RemoteViews(getPackageName(), R.layout.text_clock_widget);
+            mRemoteViews.setTextViewText(R.id.appwidget_text, time);
+
+            ComponentName mComponentName = new ComponentName(this, TextClockWidget.class);
+            AppWidgetManager mAppWidgetManager = AppWidgetManager.getInstance(this);
+            mAppWidgetManager.updateAppWidget(mComponentName, mRemoteViews);
+        }
     }
 
 
